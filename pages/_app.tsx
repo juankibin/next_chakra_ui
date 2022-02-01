d9b5585df8f7079b69044dd58e51961df0071019
juankibin <juankibin@gmail.com>
2022-02-01 23:19:29 +0800

    Collapsable Sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import {
   extendTheme,
@@ -8,6 +9,8 @@ import {
   Stack,
   IconButton,
   useDisclosure,
+  Collapse,
+  Slide,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { HamburgerIcon } from '@chakra-ui/icons';
@@ -28,45 +31,44 @@ const navigation = [
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onToggle } = useDisclosure();
+  const btnRef = React.useRef();
+  onToggle;
   return (
     <ChakraProvider theme={theme}>
       <Flex>
-        <Box borderWidth='2px' borderRadius='lg'>
-          <Stack
-            width='200px'
-            spacing={4}
-            direction='column'
-            align='center'
-            pt={20}
-          >
-            {navigation.map((item) => (
-              <Button
-                key={item.name}
-                type='button'
-                onClick={() => {
-                  router.push(item.href);
-                  navigation.forEach((x) => (x.isActive = false));
-                  item.isActive = true;
-                }}
-              >
-                {item.name}
-              </Button>
-            ))}
-          </Stack>
-        </Box>
+        <Collapse in={isOpen} animateOpacity>
+          <Box borderWidth='2px' borderRadius='lg'>
+            <Stack
+              width='200px'
+              spacing={4}
+              direction='column'
+              align='center'
+              pt={20}
+            >
+              {navigation.map((item) => (
+                <Button
+                  key={item.name}
+                  type='button'
+                  onClick={() => {
+                    router.push(item.href);
+                    navigation.forEach((x) => (x.isActive = false));
+                    item.isActive = true;
+                  }}
+                >
+                  {item.name}
+                </Button>
+              ))}
+            </Stack>
+          </Box>
+        </Collapse>
         <Stack width='100%' direction='column'>
-          <Box
-            height='50px'
-            orderWidth='2px'
-            overflow='hidden'
-            borderWidth='2px'
-          >
+          <Box height='50px' overflow='hidden' borderWidth='2px'>
             <IconButton
               borderRadius='none'
               aria-label='Search database'
               icon={<HamburgerIcon />}
-              onClick={onOpen}
+              onClick={onToggle}
             />
           </Box>
           <Component {...pageProps} />
